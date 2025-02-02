b5a8ce146d3a9d155bfc6aeca2cccf99cc5e7f1f
diggens42 <frederic.marcel.wahl@gmail.com>
2025-02-02 02:20:15 +0100

    added environment vars to .env file and removed them from docker-compose

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -4,7 +4,6 @@ set -e
 chown -R www-data:www-data /var/www/html
 chmod -R 755 /var/www/html
 
-# No need to copy wp-config.php if it's already in /var/www/html
 
 echo "Waiting for MariaDB to be ready..."
 while ! mysqladmin ping -h"$WORDPRESS_DB_HOST" --silent; do

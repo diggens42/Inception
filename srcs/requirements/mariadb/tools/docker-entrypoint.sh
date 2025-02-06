c8ba0eb4a7d06863d1bf8bf0bd7eb79d39c53f7c
diggens42 <frederic.marcel.wahl@gmail.com>
2025-02-06 19:59:38 +0100

    fixed database root setup and password issues

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -15,13 +15,18 @@ if [ ! -d "/var/lib/mysql/${DB_DATABASE}" ]; then
     done
 
     echo "Setting root password..."
-    mysqladmin -u root password "${DB_ROOT_PASSWORD}"
+    # mysqladmin -u root password "${DB_ROOT_PASSWORD}"
+    mysql -uroot <<EOF
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
+    FLUSH PRIVILEGES;
+EOF
 
     echo "Creating WordPress database and user..."
     mysql -uroot -p"${DB_ROOT_PASSWORD}" <<EOF
 CREATE DATABASE IF NOT EXISTS \`${DB_DATABASE}\`;
 CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${DB_DATABASE}\`.* TO '${DB_USER}'@'%';
+DELETE FROM mysql.user WHERE User='';
 FLUSH PRIVILEGES;
 EOF
 

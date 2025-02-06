751004fa37b96bfa42b9b457564d3d557a1ed2b3
diggens42 <frederic.marcel.wahl@gmail.com>
2025-02-06 20:27:31 +0100

    changed wp entrypoint script to use wp config and sample site is actually working now

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -9,16 +9,25 @@ if [ ! -f /var/www/html/index.php ]; then
 fi
 
 if [ ! -f /var/www/html/wp-config.php ]; then
-    echo "Creating wp-config.php from sample..."
+    echo "Creating wp-config.php from WP-CLI..."
 
-    cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+    wp config create --dbname="${DB_DATABASE}" --dbuser="${DB_USER}" --dbpass="${DB_USER_PASSWORD}" --dbhost="${DB_HOST}" --allow-root
 
-    sed -i "s/database_name_here/${DB_DATABASE}/" /var/www/html/wp-config.php
-    sed -i "s/username_here/${DB_USER}/" /var/www/html/wp-config.php
-    sed -i "s/password_here/${DB_USER_PASSWORD}/" /var/www/html/wp-config.php
-    sed -i "s/localhost/${DB_HOST}/" /var/www/html/wp-config.php
+    # cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+    # sed -i "s/database_name_here/${DB_DATABASE}/" /var/www/html/wp-config.php
+    # sed -i "s/username_here/${DB_USER}/" /var/www/html/wp-config.php
+    # sed -i "s/password_here/${DB_USER_PASSWORD}/" /var/www/html/wp-config.php
+    # sed -i "s/localhost/${DB_HOST}/" /var/www/html/wp-config.php
 
     echo "wp-config.php configured!"
+fi
+
+if ! wp core is-installed --allow-root; then
+    echo "Running WordPress installation..."
+    wp core install --url="${WP_URL}" --title="${WP_TITLE}" --admin_user="${WP_ADMIN_USER}" --admin_password="${WP_ADMIN_PASSWORD}" --admin_email="${WP_ADMIN_EMAIL}" --allow-root
+else
+    echo "WordPress already installed. Skipping installation."
 fi
 
 exec "$@"

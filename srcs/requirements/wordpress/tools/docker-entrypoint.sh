f856cad4ae5d502fad85cdc9f906ffd98ca34e80
diggens42 <frederic.marcel.wahl@gmail.com>
2025-02-07 17:26:34 +0100

    reworked healthcheck for wordpress to check if php-fpm process is running

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -13,13 +13,6 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 
     wp config create --dbname="${DB_DATABASE}" --dbuser="${DB_USER}" --dbpass="${DB_USER_PASSWORD}" --dbhost="${DB_HOST}" --allow-root
 
-    # cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-
-    # sed -i "s/database_name_here/${DB_DATABASE}/" /var/www/html/wp-config.php
-    # sed -i "s/username_here/${DB_USER}/" /var/www/html/wp-config.php
-    # sed -i "s/password_here/${DB_USER_PASSWORD}/" /var/www/html/wp-config.php
-    # sed -i "s/localhost/${DB_HOST}/" /var/www/html/wp-config.php
-
     echo "wp-config.php configured!"
 fi
 
